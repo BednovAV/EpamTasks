25ad73f7c7ffcfb822b6ec27be3a10fd531d4918
Andrey <bednov_av@mail.ru>
2021-03-20 17:40:10 +0400

    Task 2.2.1 Added game objects

--- Task 2/Task 2.2/Task2.2.1.GameLogic/Entities/Bunuses/MagicPotion.cs
+++ Task 2/Task 2.2/Task2.2.1.GameLogic/Entities/Bunuses/MagicPotion.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Task2._2._1.GameLogic.Interfaces;
 
 namespace Task2._2._1.GameLogic.Entities.Bunuses
 {
-    public abstract class Bonus : GameObject, IBonus
+    public class MagicPotion : Bonus
     {
-        public abstract int BonusHP { get; }
+        public override int BonusHP => throw new NotImplementedException();
     }
 }
