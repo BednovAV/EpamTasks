df646286acbb7a5da169ed70d2f16bcc1e739ce1
Andrey <bednov_av@mail.ru>
2021-03-23 21:58:35 +0400

    Task 3.2.1 added AddRange(IEnumerable<T> ) method

--- Task 3/Task 3.2/Task3.2.1.TestPlace/Program.cs
+++ Task 3/Task 3.2/Task3.2.1.TestPlace/Program.cs
@@ -18,11 +18,17 @@ namespace Task3._2._1.TestPlace
 
             for (int i = 0; i < test2.Length; i++)
             {
-                Console.WriteLine($"i = {test2[i]} Length = {test2.Length} Capacity = {test2.Capacity}");
+                //Console.WriteLine($"i = {test2[i]} Length = {test2.Length} Capacity = {test2.Capacity}");
             }
 
             test2.Add(222);
-            Console.WriteLine($"i = {test2[20]} Length = {test2.Length} Capacity = {test2.Capacity}");
+            //Console.WriteLine($"i = {test2[20]} Length = {test2.Length} Capacity = {test2.Capacity}");
+
+            test2.AddRange(test1);
+            for (int i = 0; i < test2.Length; i++)
+            {
+                Console.WriteLine($"i = {test2[i]} Length = {test2.Length} Capacity = {test2.Capacity}");
+            }
         }
     }
 }
