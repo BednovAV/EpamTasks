348d72594de514bef4084000bc2d8f5954b00cbf
Andrey <bednov_av@mail.ru>
2021-03-26 00:28:53 +0400

    Task 3.1.1 Added base of CycledLinkedList

---
+++ Task 3/Task 3.1/Task 3.1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new CycledLinkedList(10);
+
+            foreach (var item in test.GetValues())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
